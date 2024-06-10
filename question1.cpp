b4d93d6e8fe4d9e943d14dd7cc03fcbdaa57d2b6
itaintkevin <kpjtofficial@gmail.com>
2024-06-10 18:36:01 +0530

    added solution to problem

---
+++ question1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+// let T be testcases 
+// let N be numDishes
+// let K be numCatogeries
+
+
+
+int main() {
+    int testcases;
+    int numDishes, numCatogeries;
+    int categories[] = {0};
+    int timeToCook[] = {0};
+    int minTime;
+    cin >> testcases;
+    while (testcases > 0) {
+        cin >> numDishes >> numCatogeries;
+        for (int i = 0; i < numDishes; i++) {
+            cin >> categories[i];
+        } 
+        for (int i = 0; i < numDishes; i++) {
+            cin >> timeToCook[i];
+        } 
+
+        for (int i = 0; i < numCatogeries; i++) {
+            minTime = timeToCook[i];
+            for (int j = 0; j < numDishes; j++) {
+                if (categories[j] == i) {
+                    if (timeToCook[j] < minTime) {
+                        minTime = timeToCook[j];
+                    }
+                }
+            }
+            cout << minTime << " ";
+        }
+    }
+   
+}
